59f8069210eb6bc612f7961e0e25afd9d2309c91
Shinichi Yagi <syusuke@ogino@gmail.com>
2019-09-18 14:09:37 +0900

    Add message box alert.

--- BinaryOptionTrade.py
+++ BinaryOptionTrade.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import messagebox
-import Parameter
 import Scrape
 
 root = tk.Tk()
@@ -13,10 +12,24 @@ def main():
     while condition_ok is False:
         messagebox.showwarning("注意", "必ずメインのメニューから取引する項目を"
                                      "「HighLow・HighLowスプレッド・Turbo・Turboスプレッド」"
-                                     "のいずれかから選択しておいて下さい。")
+                                     "のいずれかから選択しておいて下さい。またメインメニューでいずれを選択した場合でも"
+                                     "２段目のメニューで取引時間（間隔）を指定して下さい。")
         selected_menu, selected_term = Scrape.get_selected_menu_and_term()
+        if selected_menu == "ChangingStrike":
+            messagebox.showwarning("注意", "メインメニューでHighLowを選択した場合、２段目のメニューで"
+                                         "取引時間（間隔）を指定して下さい")
+        elif selected_menu == "FixedPayoutHL":
+            messagebox.showwarning("注意", "メインメニューでHighLowスプレッドを選択した場合でも、２段目のメニューで"
+                                         "取引時間（間隔）を指定して下さい")
+        elif selected_menu == "ChangingStrikeOOD":
+            messagebox.showwarning("注意", "メインメニューでTurboを選択した場合でも、２段目のメニューで"
+                                         "取引時間（間隔）を指定して下さい")
+        elif selected_menu == "FixedPayoutHLOOD":
+            messagebox.showwarning("注意", "メインメニューでTurboスプレッドを選択した場合でも、２段目のメニューで"
+                                         "取引時間（間隔）を指定して下さい")
         print("selected_menu=" + selected_menu + "    selected_term=" + selected_term)
-        if selected_term is not "全て":
+        if selected_term != "全て":
+            Scrape.set_oneclick_trade()
             condition_ok = True
             Scrape.wait_signal()
         else:
