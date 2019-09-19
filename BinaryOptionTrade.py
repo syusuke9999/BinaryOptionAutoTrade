4c127965ee9c8464f7dd196348f1f48da9e14758
Shinichi Yagi <syusuke@ogino@gmail.com>
2019-09-19 13:31:50 +0900

    Change variable name from selected term to selected duration.

--- BinaryOptionTrade.py
+++ BinaryOptionTrade.py
@@ -14,7 +14,7 @@ def main():
                                      "「HighLow・HighLowスプレッド・Turbo・Turboスプレッド」"
                                      "のいずれかから選択しておいて下さい。またメインメニューでいずれを選択した場合でも"
                                      "２段目のメニューで取引時間（間隔）を指定して下さい。")
-        selected_menu, selected_term = Scrape.get_selected_menu_and_term()
+        selected_menu, selected_duration = Scrape.get_selected_menu_and_duration()
         if selected_menu == "ChangingStrike":
             messagebox.showwarning("注意", "メインメニューでHighLowを選択した場合、２段目のメニューで"
                                          "取引時間（間隔）を指定して下さい")
@@ -27,8 +27,9 @@ def main():
         elif selected_menu == "FixedPayoutHLOOD":
             messagebox.showwarning("注意", "メインメニューでTurboスプレッドを選択した場合でも、２段目のメニューで"
                                          "取引時間（間隔）を指定して下さい")
-        print("selected_menu=" + selected_menu + "    selected_term=" + selected_term)
-        if selected_term != "全て":
+        print("selected_menu=" + selected_menu + "    selected_duration=" + selected_duration)
+        if selected_duration != "全て":
+
             Scrape.set_oneclick_trade()
             condition_ok = True
             Scrape.wait_signal()
