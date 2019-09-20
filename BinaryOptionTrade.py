c000d5bd923be29f12aa67192cd55f687e31ce5f
Shinichi Yagi <syusuke@ogino@gmail.com>
2019-09-20 13:40:16 +0900

    Improve warning massage.

--- BinaryOptionTrade.py
+++ BinaryOptionTrade.py
@@ -8,31 +8,20 @@ root.withdraw()
 
 def main():
     login()
+    messagebox.showwarning("注意", "必ずメインのメニューから取引する項目を"
+                                 "「HighLow・HighLowスプレッド・Turbo・Turboスプレッド」"
+                                 "のいずれかから選択しておいて下さい。またメインメニューでいずれを選択した場合でも"
+                                 "２段目のメニューで取引時間（間隔）を指定して下さい。")
     condition_ok = False
     while condition_ok is False:
-        messagebox.showwarning("注意", "必ずメインのメニューから取引する項目を"
-                                     "「HighLow・HighLowスプレッド・Turbo・Turboスプレッド」"
-                                     "のいずれかから選択しておいて下さい。またメインメニューでいずれを選択した場合でも"
-                                     "２段目のメニューで取引時間（間隔）を指定して下さい。")
         selected_menu, selected_duration = Scrape.get_selected_menu_and_duration()
-        if selected_menu == "ChangingStrike":
-            messagebox.showwarning("注意", "メインメニューでHighLowを選択した場合、２段目のメニューで"
-                                         "取引時間（間隔）を指定して下さい")
-        elif selected_menu == "FixedPayoutHL":
-            messagebox.showwarning("注意", "メインメニューでHighLowスプレッドを選択した場合でも、２段目のメニューで"
-                                         "取引時間（間隔）を指定して下さい")
-        elif selected_menu == "ChangingStrikeOOD":
-            messagebox.showwarning("注意", "メインメニューでTurboを選択した場合でも、２段目のメニューで"
-                                         "取引時間（間隔）を指定して下さい")
-        elif selected_menu == "FixedPayoutHLOOD":
-            messagebox.showwarning("注意", "メインメニューでTurboスプレッドを選択した場合でも、２段目のメニューで"
-                                         "取引時間（間隔）を指定して下さい")
         print("selected_menu=" + selected_menu + "    selected_duration=" + selected_duration)
         if selected_duration != "全て":
             Scrape.set_oneclick_trade()
             condition_ok = True
             Scrape.wait_signal()
         else:
+            messagebox.showwarning("注意", "取引期間が選択されていません。取引期間を選択してからOKをクリックして下さい。")
             continue
 
 
